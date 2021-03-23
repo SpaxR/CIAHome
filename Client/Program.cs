70b6e010bbae6f7f4759e8ba840e29273cc2cc18
SpaxR <55739719+SpaxR@users.noreply.github.com>
2021-03-23 11:44:44 +0100

    Replaced MatBlazor with MudBlazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,24 +1,27 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using MatBlazor;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 
 namespace CIAHome.Client
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped(_ => new HttpClient
+										   {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
-            builder.Services.AddMatBlazor();
-            
-            await builder.Build().RunAsync();
-        }
-    }
+			builder.Services.AddMudServices();
+			builder.Services.AddMudBlazorDialog();
+			builder.Services.AddMudBlazorSnackbar();
+
+			await builder.Build().RunAsync();
+		}
+	}
 }
