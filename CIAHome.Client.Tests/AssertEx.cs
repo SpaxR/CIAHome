b676598e4372af815ac90459356943d2e425dae4
SpaxR <55739719+SpaxR@users.noreply.github.com>
2021-04-06 00:38:40 +0200

    Added Tests for TodoListCard.razor

---
+++ CIAHome.Client.Tests/AssertEx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace CIAHome.Client.Tests
+{
+	public static class AssertEx
+	{
+		public static EventCallback<T> AsCallback<T>(this EventHandler<T> handler)
+		{
+			return EventCallback.Factory
+								.Create<T>(
+									handler,
+									args => handler.Invoke(null, args));
+		}
+	}
+}
---
+++ CIAHome.Client.Tests/Components/TodoListCardSpec.cs
@@ -0,0 +1,58 @@
+﻿using Bunit;
+using CIAHome.Client.Components.Cards;
+using CIAHome.Shared.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor;
+using Xunit;
+
+namespace CIAHome.Client.Tests.Components
+{
+	public class TodoListCardSpec : TestContext
+	{
+		private IRenderedComponent<TodoListCard> _sut;
+
+		private IRenderedComponent<TodoListCard> SUT
+		{
+			get
+			{
+				_sut ??= RenderComponent<TodoListCard>();
+				return _sut;
+			}
+		}
+
+		[Fact]
+		public void shows_text_of_list()
+		{
+			var list = new TodoList();
+			SUT.SetParametersAndRender((nameof(TodoListCard.List), list));
+
+			Assert.Contains(list.Text, SUT.Markup);
+		}
+
+		[Fact]
+		public void MissingList_shows_MudSkeleton()
+		{
+			SUT.FindComponent<MudSkeleton>();
+		}
+
+		[Fact]
+		public void contains_delete_IconButton()
+		{
+			var button = SUT.FindComponent<MudIconButton>();
+
+			Assert.Equal(Icons.Filled.Delete, button.Instance.Icon);
+		}
+
+		[Fact]
+		public void delete_IconButton_calls_OnDelete()
+		{
+			var button = SUT.FindComponent<MudIconButton>();
+
+			Assert.Raises<MouseEventArgs>(
+				handler => SUT.SetParametersAndRender((nameof(TodoListCard.OnDelete), handler.AsCallback())),
+				_ => SUT.Instance.OnDelete = EventCallback<MouseEventArgs>.Empty,
+				() => button.Find("button").Click());
+		}
+	}
+}
