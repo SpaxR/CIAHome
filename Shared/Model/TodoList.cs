b368b6496af51cd9e75525b648a656d9f534c887
SpaxR <55739719+SpaxR@users.noreply.github.com>
2021-04-09 12:45:55 +0200

    Fixed serialization of Todo-List

--- Shared/Model/TodoList.cs
+++ Shared/Model/TodoList.cs
@@ -5,8 +5,8 @@ namespace CIAHome.Shared.Model
 {
 	public class TodoList
 	{
-		public          string      Id   { get; set; } = Guid.NewGuid().ToString("N");
-		public          string      Text { get; set; } = "New Todo-List";
-		public readonly IList<Todo> Todos = new List<Todo>();
+		public string      Id    { get; set; } = Guid.NewGuid().ToString("N");
+		public string      Text  { get; set; } = "New Todo-List";
+		public IList<Todo> Todos { get; set; } = new List<Todo>();
 	}
 }
