3f281da87f9e714e0db6f864946d3457c5321d86
SpaxR <55739719+SpaxR@users.noreply.github.com>
2021-05-10 21:55:35 +0200

    Added Gtin to ProductCard

--- CIAHome.Client.Tests/Tests/Components/Product/ProductCardSpec.cs
+++ CIAHome.Client.Tests/Tests/Components/Product/ProductCardSpec.cs
@@ -1,5 +1,7 @@
 using Bunit;
 using CIAHome.Client.Components;
+using CIAHome.Client.Tests.PageModel;
+using Xunit;
 
 namespace CIAHome.Client.Tests
 {
@@ -7,15 +9,25 @@ namespace CIAHome.Client.Tests
 	{
 		private readonly CIAHome.Shared.Model.Product _product = new();
 
-		private IRenderedComponent<ProductCard> _sut;
+		private ProductCardComponent _sut;
 
-		private IRenderedComponent<ProductCard> SUT
+		private ProductCardComponent SUT
 		{
 			get
 			{
-				_sut ??= RenderComponent<ProductCard>((nameof(ProductCard.Product), _product));
+				_sut ??= new ProductCardComponent(RenderComponent<ProductCard>(
+													  (nameof(ProductCard.Product), _product)));
 				return _sut;
 			}
+		}
+
+		[Fact]
+		public void contains_TextField_with_Gtin()
+		{
+			_product.GTIN = "SOME GTIN";
+
+			Assert.Equal(_product.GTIN, SUT.GtinTextField.Find("input").GetAttribute("value"));
+			
 		}
 	}
 }
--- CIAHome.Client.Tests/src/PageModel/Pantry/ProductCardComponent.cs
+++ CIAHome.Client.Tests/src/PageModel/Pantry/ProductCardComponent.cs
@@ -1,10 +1,14 @@
 using Bunit;
 using CIAHome.Client.Components;
+using MudBlazor;
 
 namespace CIAHome.Client.Tests.PageModel
 {
 	public class ProductCardComponent : ComponentBase<ProductCard>
 	{
+		public IRenderedComponent<MudTextField<string>> GtinTextField => Root.FindComponent<MudTextField<string>>();
+
+
 		/// <inheritdoc />
 		public ProductCardComponent(IRenderedComponent<ProductCard> root) : base(root) { }
 	}
