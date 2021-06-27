98307dad0bd7813d4bd5aa6b11759cc8769762f4
SpaxR <55739719+SpaxR@users.noreply.github.com>
2021-06-27 20:10:02 +0200

    Fixed/Added Log to Integration-Test

--- Tests.Integration/PumpControlSpec.cs
+++ Tests.Integration/PumpControlSpec.cs
@@ -12,17 +12,25 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using MudBlazor.Services;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Tests.Integration
 {
 	public class PumpControlSpec : TestContext, IClassFixture<WebApplicationFactory<Startup>>
 	{
-		private readonly HubConnection _dataProvider;
+		private readonly ITestOutputHelper               _testConsole;
+		private readonly HubConnection                   _dataProvider;
+		private readonly Mock<ILogger<PumpControlProxy>> _loggerMock = new();
+
 
 		/// <inheritdoc />
-		public PumpControlSpec(WebApplicationFactory<Startup> factory)
+		public PumpControlSpec(ITestOutputHelper testConsole, WebApplicationFactory<Startup> factory)
 		{
+			_testConsole = testConsole;
 			TestServer server = factory.Server;
 			server.BaseAddress = new Uri("http://localhost");
 
@@ -30,14 +38,20 @@ namespace Tests.Integration
 							.WithUrl(new Uri(server.BaseAddress, "/hubs/pumpcontrol"),
 									 options => options.HttpMessageHandlerFactory = _ => server.CreateHandler())
 							.Build();
+			testConsole.WriteLine("Build HubConnection for DataProvider");
 
 			var proxyConnection = new HubConnectionBuilder()
 								  .WithUrl(new Uri(server.BaseAddress, "/hubs/pumpcontrol"),
 										   options => options.HttpMessageHandlerFactory = _ => server.CreateHandler())
 								  .Build();
+			testConsole.WriteLine("Build HubConnection for SUT-Proxy");
 
+
+			Services.AddMudServices();
 			Services.AddSingleton(server.CreateClient());
-			Services.AddScoped<IPumpControlService>(_ => new PumpControlProxy(proxyConnection));
+			Services.AddScoped<IPumpControlService>(_ => new PumpControlProxy(_loggerMock.Object, proxyConnection));
+			testConsole.WriteLine("Added Services");
+			testConsole.WriteLine(" - - - - - - - - - - ");
 		}
 
 		[Fact]
@@ -46,25 +60,26 @@ namespace Tests.Integration
 			// Arrange
 			var watertankStatus = new WatertankStatus {VolumeFilled = 25, VolumeTotal = 100};
 			var pumpStatus      = new PumpStatus {IsRunning         = true};
-
 			var pumpControlComp = RenderComponent<PumpControl>();
+			_testConsole.WriteLine("Arrange - Created References");
 
 			// Act - Send new Data
 			await _dataProvider.StartAsync();
 			await _dataProvider.InvokeAsync(nameof(PumpControlHub.UpdateWatertank), watertankStatus);
 			await _dataProvider.InvokeAsync(nameof(PumpControlHub.UpdatePump),      pumpStatus);
 			await _dataProvider.StopAsync();
+			_testConsole.WriteLine("Act - Send new Data to Client");
 
 			// Assert
 			pumpControlComp.WaitForAssertion(
-				() => Assert.Equal(
-					100 / watertankStatus.VolumeTotal * watertankStatus.VolumeFilled,
-					pumpControlComp.FindComponent<TankImage>().Instance.FilledPercentage,
-					2), TimeSpan.FromSeconds(3));
+				() => Assert.Equal(25d, pumpControlComp.FindComponent<TankImage>().Instance.FilledPercentage),
+				TimeSpan.FromSeconds(3));
+			_testConsole.WriteLine("Assert -  Watertank gets updated");
 
-			// pumpControlComp.WaitForAssertion(
-			// 	() => Assert.Equal(true, pumpControlComp.FindComponent<PumpImage>().Instance.IsRunning),
-			// 	TimeSpan.FromSeconds(3));
+			pumpControlComp.WaitForAssertion(
+				() => Assert.Equal(true, pumpControlComp.FindComponent<PumpImage>().Instance.IsRunning),
+				TimeSpan.FromSeconds(3));
+			_testConsole.WriteLine("Assert -  Pump gets updated");
 		}
 	}
 }
